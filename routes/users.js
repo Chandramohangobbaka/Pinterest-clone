4f3cacdc83aaa72261202808c1243174fe464349
Chandramohangobbaka <gobbaka2004chandra@gmail.com>
2024-05-10 13:39:52 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/Pinterest");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+  },
+  posts: [{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Post'
+  }],
+  dp: {
+    type: String,
+    default: 'default_dp.jpg' // You can set a default profile picture
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  fullname: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.plugin(plm);
+
+module.exports  = mongoose.model('User', userSchema);
+
+
+
